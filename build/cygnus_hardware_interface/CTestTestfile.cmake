11eaa7d0bec14a1cd74c91fe0b79c9fdc97a03cd
Carlos Sanoja <wuopcarlos@gmail.com>
2018-06-24 13:29:15 -0700

    build act

---
+++ build/cygnus_hardware_interface/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Documents/cygnus_ws/src/cygnus_hardware_interface
+# Build directory: /home/robond/Documents/cygnus_ws/build/cygnus_hardware_interface
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
