c04e996d96f7456f510d13d5bf0532d07ff4e811
Carlos Sanoja <wuopcarlos@gmail.com>
2018-08-30 07:17:09 -0700

    CHORE: Updating folders in repository

--- build/pysdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Documents/cygnus_ws/src/pysdf
-# Build directory: /home/robond/Documents/cygnus_ws/build/pysdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
